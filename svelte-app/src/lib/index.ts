72fb580b2d1c38c36dd32a2143fab36f73c92c7e
jlh7 <j.lh.t@outlook.com>
2024-09-07 22:57:50 +0700

    Update

---
+++ svelte-app/src/lib/index.ts
@@ -0,0 +1,646 @@
+// place files you want to import through the `$lib` alias in this folder.
+export const elements = [
+  { symbol: "H", name: "Hydrogen", atomicNumber: 1, group: 1, period: 1, type: "nonmetal" },
+  { symbol: "He", name: "Helium", atomicNumber: 2, group: 18, period: 1, type: "noble gas" },
+  { symbol: "Li", name: "Lithium", atomicNumber: 3, group: 1, period: 2, type: "alkali metal" },
+  {
+    symbol: "Be",
+    name: "Beryllium",
+    atomicNumber: 4,
+    group: 2,
+    period: 2,
+    type: "alkaline earth metal"
+  },
+  { symbol: "B", name: "Boron", atomicNumber: 5, group: 13, period: 2, type: "metalloid" },
+  { symbol: "C", name: "Carbon", atomicNumber: 6, group: 14, period: 2, type: "nonmetal" },
+  { symbol: "N", name: "Nitrogen", atomicNumber: 7, group: 15, period: 2, type: "nonmetal" },
+  { symbol: "O", name: "Oxygen", atomicNumber: 8, group: 16, period: 2, type: "nonmetal" },
+  { symbol: "F", name: "Fluorine", atomicNumber: 9, group: 17, period: 2, type: "nonmetal" },
+  { symbol: "Ne", name: "Neon", atomicNumber: 10, group: 18, period: 2, type: "noble gas" },
+  { symbol: "Na", name: "Sodium", atomicNumber: 11, group: 1, period: 3, type: "alkali metal" },
+  {
+    symbol: "Mg",
+    name: "Magnesium",
+    atomicNumber: 12,
+    group: 2,
+    period: 3,
+    type: "alkaline earth metal"
+  },
+  {
+    symbol: "Al",
+    name: "Aluminum",
+    atomicNumber: 13,
+    group: 13,
+    period: 3,
+    type: "post-transition metal"
+  },
+  { symbol: "Si", name: "Silicon", atomicNumber: 14, group: 14, period: 3, type: "metalloid" },
+  { symbol: "P", name: "Phosphorus", atomicNumber: 15, group: 15, period: 3, type: "nonmetal" },
+  { symbol: "S", name: "Sulfur", atomicNumber: 16, group: 16, period: 3, type: "nonmetal" },
+  { symbol: "Cl", name: "Chlorine", atomicNumber: 17, group: 17, period: 3, type: "nonmetal" },
+  { symbol: "Ar", name: "Argon", atomicNumber: 18, group: 18, period: 3, type: "noble gas" },
+  { symbol: "K", name: "Potassium", atomicNumber: 19, group: 1, period: 4, type: "alkali metal" },
+  {
+    symbol: "Ca",
+    name: "Calcium",
+    atomicNumber: 20,
+    group: 2,
+    period: 4,
+    type: "alkaline earth metal"
+  },
+  {
+    symbol: "Sc",
+    name: "Scandium",
+    atomicNumber: 21,
+    group: 3,
+    period: 4,
+    type: "transition metal"
+  },
+  {
+    symbol: "Ti",
+    name: "Titanium",
+    atomicNumber: 22,
+    group: 4,
+    period: 4,
+    type: "transition metal"
+  },
+  {
+    symbol: "V",
+    name: "Vanadium",
+    atomicNumber: 23,
+    group: 5,
+    period: 4,
+    type: "transition metal"
+  },
+  {
+    symbol: "Cr",
+    name: "Chromium",
+    atomicNumber: 24,
+    group: 6,
+    period: 4,
+    type: "transition metal"
+  },
+  {
+    symbol: "Mn",
+    name: "Manganese",
+    atomicNumber: 25,
+    group: 7,
+    period: 4,
+    type: "transition metal"
+  },
+  { symbol: "Fe", name: "Iron", atomicNumber: 26, group: 8, period: 4, type: "transition metal" },
+  { symbol: "Co", name: "Cobalt", atomicNumber: 27, group: 9, period: 4, type: "transition metal" },
+  {
+    symbol: "Ni",
+    name: "Nickel",
+    atomicNumber: 28,
+    group: 10,
+    period: 4,
+    type: "transition metal"
+  },
+  {
+    symbol: "Cu",
+    name: "Copper",
+    atomicNumber: 29,
+    group: 11,
+    period: 4,
+    type: "transition metal"
+  },
+  { symbol: "Zn", name: "Zinc", atomicNumber: 30, group: 12, period: 4, type: "transition metal" },
+  {
+    symbol: "Ga",
+    name: "Gallium",
+    atomicNumber: 31,
+    group: 13,
+    period: 4,
+    type: "post-transition metal"
+  },
+  { symbol: "Ge", name: "Germanium", atomicNumber: 32, group: 14, period: 4, type: "metalloid" },
+  { symbol: "As", name: "Arsenic", atomicNumber: 33, group: 15, period: 4, type: "metalloid" },
+  { symbol: "Se", name: "Selenium", atomicNumber: 34, group: 16, period: 4, type: "nonmetal" },
+  { symbol: "Br", name: "Bromine", atomicNumber: 35, group: 17, period: 4, type: "nonmetal" },
+  { symbol: "Kr", name: "Krypton", atomicNumber: 36, group: 18, period: 4, type: "noble gas" },
+  { symbol: "Rb", name: "Rubidium", atomicNumber: 37, group: 1, period: 5, type: "alkali metal" },
+  {
+    symbol: "Sr",
+    name: "Strontium",
+    atomicNumber: 38,
+    group: 2,
+    period: 5,
+    type: "alkaline earth metal"
+  },
+  { symbol: "Y", name: "Yttrium", atomicNumber: 39, group: 3, period: 5, type: "transition metal" },
+  {
+    symbol: "Zr",
+    name: "Zirconium",
+    atomicNumber: 40,
+    group: 4,
+    period: 5,
+    type: "transition metal"
+  },
+  {
+    symbol: "Nb",
+    name: "Niobium",
+    atomicNumber: 41,
+    group: 5,
+    period: 5,
+    type: "transition metal"
+  },
+  {
+    symbol: "Mo",
+    name: "Molybdenum",
+    atomicNumber: 42,
+    group: 6,
+    period: 5,
+    type: "transition metal"
+  },
+  {
+    symbol: "Tc",
+    name: "Technetium",
+    atomicNumber: 43,
+    group: 7,
+    period: 5,
+    type: "transition metal"
+  },
+  {
+    symbol: "Ru",
+    name: "Ruthenium",
+    atomicNumber: 44,
+    group: 8,
+    period: 5,
+    type: "transition metal"
+  },
+  {
+    symbol: "Rh",
+    name: "Rhodium",
+    atomicNumber: 45,
+    group: 9,
+    period: 5,
+    type: "transition metal"
+  },
+  {
+    symbol: "Pd",
+    name: "Palladium",
+    atomicNumber: 46,
+    group: 10,
+    period: 5,
+    type: "transition metal"
+  },
+  {
+    symbol: "Ag",
+    name: "Silver",
+    atomicNumber: 47,
+    group: 11,
+    period: 5,
+    type: "transition metal"
+  },
+  {
+    symbol: "Cd",
+    name: "Cadmium",
+    atomicNumber: 48,
+    group: 12,
+    period: 5,
+    type: "transition metal"
+  },
+  {
+    symbol: "In",
+    name: "Indium",
+    atomicNumber: 49,
+    group: 13,
+    period: 5,
+    type: "post-transition metal"
+  },
+  {
+    symbol: "Sn",
+    name: "Tin",
+    atomicNumber: 50,
+    group: 14,
+    period: 5,
+    type: "post-transition metal"
+  },
+  { symbol: "Sb", name: "Antimony", atomicNumber: 51, group: 15, period: 5, type: "metalloid" },
+  { symbol: "Te", name: "Tellurium", atomicNumber: 52, group: 16, period: 5, type: "metalloid" },
+  { symbol: "I", name: "Iodine", atomicNumber: 53, group: 17, period: 5, type: "nonmetal" },
+  { symbol: "Xe", name: "Xenon", atomicNumber: 54, group: 18, period: 5, type: "noble gas" },
+  { symbol: "Cs", name: "Cesium", atomicNumber: 55, group: 1, period: 6, type: "alkali metal" },
+  {
+    symbol: "Ba",
+    name: "Barium",
+    atomicNumber: 56,
+    group: 2,
+    period: 6,
+    type: "alkaline earth metal"
+  },
+  { symbol: "La", name: "Lanthanum", atomicNumber: 57, group: 3, period: 6, type: "lanthanide" },
+  {
+    symbol: "Ce",
+    name: "Cerium",
+    atomicNumber: 58,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Pr",
+    name: "Praseodymium",
+    atomicNumber: 59,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Nd",
+    name: "Neodymium",
+    atomicNumber: 60,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Pm",
+    name: "Promethium",
+    atomicNumber: 61,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Sm",
+    name: "Samarium",
+    atomicNumber: 62,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Eu",
+    name: "Europium",
+    atomicNumber: 63,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Gd",
+    name: "Gadolinium",
+    atomicNumber: 64,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Tb",
+    name: "Terbium",
+    atomicNumber: 65,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Dy",
+    name: "Dysprosium",
+    atomicNumber: 66,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Ho",
+    name: "Holmium",
+    atomicNumber: 67,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Er",
+    name: "Erbium",
+    atomicNumber: 68,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Tm",
+    name: "Thulium",
+    atomicNumber: 69,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  {
+    symbol: "Yb",
+    name: "Ytterbium",
+    atomicNumber: 70,
+    group: "lanthanides",
+    period: 6,
+    type: "lanthanide"
+  },
+  { symbol: "Lu", name: "Lutetium", atomicNumber: 71, group: 3, period: 6, type: "lanthanide" },
+  {
+    symbol: "Hf",
+    name: "Hafnium",
+    atomicNumber: 72,
+    group: 4,
+    period: 6,
+    type: "transition metal"
+  },
+  {
+    symbol: "Ta",
+    name: "Tantalum",
+    atomicNumber: 73,
+    group: 5,
+    period: 6,
+    type: "transition metal"
+  },
+  {
+    symbol: "W",
+    name: "Tungsten",
+    atomicNumber: 74,
+    group: 6,
+    period: 6,
+    type: "transition metal"
+  },
+  {
+    symbol: "Re",
+    name: "Rhenium",
+    atomicNumber: 75,
+    group: 7,
+    period: 6,
+    type: "transition metal"
+  },
+  { symbol: "Os", name: "Osmium", atomicNumber: 76, group: 8, period: 6, type: "transition metal" },
+  {
+    symbol: "Ir",
+    name: "Iridium",
+    atomicNumber: 77,
+    group: 9,
+    period: 6,
+    type: "transition metal"
+  },
+  {
+    symbol: "Pt",
+    name: "Platinum",
+    atomicNumber: 78,
+    group: 10,
+    period: 6,
+    type: "transition metal"
+  },
+  { symbol: "Au", name: "Gold", atomicNumber: 79, group: 11, period: 6, type: "transition metal" },
+  {
+    symbol: "Hg",
+    name: "Mercury",
+    atomicNumber: 80,
+    group: 12,
+    period: 6,
+    type: "transition metal"
+  },
+  {
+    symbol: "Tl",
+    name: "Thallium",
+    atomicNumber: 81,
+    group: 13,
+    period: 6,
+    type: "post-transition metal"
+  },
+  {
+    symbol: "Pb",
+    name: "Lead",
+    atomicNumber: 82,
+    group: 14,
+    period: 6,
+    type: "post-transition metal"
+  },
+  {
+    symbol: "Bi",
+    name: "Bismuth",
+    atomicNumber: 83,
+    group: 15,
+    period: 6,
+    type: "post-transition metal"
+  },
+  { symbol: "Po", name: "Polonium", atomicNumber: 84, group: 16, period: 6, type: "metalloid" },
+  { symbol: "At", name: "Astatine", atomicNumber: 85, group: 17, period: 6, type: "metalloid" },
+  { symbol: "Rn", name: "Radon", atomicNumber: 86, group: 18, period: 6, type: "noble gas" },
+  { symbol: "Fr", name: "Francium", atomicNumber: 87, group: 1, period: 7, type: "alkali metal" },
+  {
+    symbol: "Ra",
+    name: "Radium",
+    atomicNumber: 88,
+    group: 2,
+    period: 7,
+    type: "alkaline earth metal"
+  },
+  { symbol: "Ac", name: "Actinium", atomicNumber: 89, group: 3, period: 7, type: "actinide" },
+  {
+    symbol: "Th",
+    name: "Thorium",
+    atomicNumber: 90,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Pa",
+    name: "Protactinium",
+    atomicNumber: 91,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "U",
+    name: "Uranium",
+    atomicNumber: 92,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Np",
+    name: "Neptunium",
+    atomicNumber: 93,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Pu",
+    name: "Plutonium",
+    atomicNumber: 94,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Am",
+    name: "Americium",
+    atomicNumber: 95,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Cm",
+    name: "Curium",
+    atomicNumber: 96,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Bk",
+    name: "Berkelium",
+    atomicNumber: 97,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Cf",
+    name: "Californium",
+    atomicNumber: 98,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Es",
+    name: "Einsteinium",
+    atomicNumber: 99,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Fm",
+    name: "Fermium",
+    atomicNumber: 100,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "Md",
+    name: "Mendelevium",
+    atomicNumber: 101,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  {
+    symbol: "No",
+    name: "Nobelium",
+    atomicNumber: 102,
+    group: "actinides",
+    period: 7,
+    type: "actinide"
+  },
+  { symbol: "Lr", name: "Lawrencium", atomicNumber: 103, group: 3, period: 7, type: "actinide" },
+  {
+    symbol: "Rf",
+    name: "Rutherfordium",
+    atomicNumber: 104,
+    group: 4,
+    period: 7,
+    type: "transition metal"
+  },
+  {
+    symbol: "Db",
+    name: "Dubnium",
+    atomicNumber: 105,
+    group: 5,
+    period: 7,
+    type: "transition metal"
+  },
+  {
+    symbol: "Sg",
+    name: "Seaborgium",
+    atomicNumber: 106,
+    group: 6,
+    period: 7,
+    type: "transition metal"
+  },
+  {
+    symbol: "Bh",
+    name: "Bohrium",
+    atomicNumber: 107,
+    group: 7,
+    period: 7,
+    type: "transition metal"
+  },
+  {
+    symbol: "Hs",
+    name: "Hassium",
+    atomicNumber: 108,
+    group: 8,
+    period: 7,
+    type: "transition metal"
+  },
+  {
+    symbol: "Mt",
+    name: "Meitnerium",
+    atomicNumber: 109,
+    group: 9,
+    period: 7,
+    type: "transition metal"
+  },
+  {
+    symbol: "Ds",
+    name: "Darmstadtium",
+    atomicNumber: 110,
+    group: 10,
+    period: 7,
+    type: "transition metal"
+  },
+  {
+    symbol: "Rg",
+    name: "Roentgenium",
+    atomicNumber: 111,
+    group: 11,
+    period: 7,
+    type: "transition metal"
+  },
+  {
+    symbol: "Cn",
+    name: "Copernicium",
+    atomicNumber: 112,
+    group: 12,
+    period: 7,
+    type: "transition metal"
+  },
+  {
+    symbol: "Nh",
+    name: "Nihonium",
+    atomicNumber: 113,
+    group: 13,
+    period: 7,
+    type: "post-transition metal"
+  },
+  {
+    symbol: "Fl",
+    name: "Flerovium",
+    atomicNumber: 114,
+    group: 14,
+    period: 7,
+    type: "post-transition metal"
+  },
+  {
+    symbol: "Mc",
+    name: "Moscovium",
+    atomicNumber: 115,
+    group: 15,
+    period: 7,
+    type: "post-transition metal"
+  },
+  {
+    symbol: "Lv",
+    name: "Livermorium",
+    atomicNumber: 116,
+    group: 16,
+    period: 7,
+    type: "post-transition metal"
+  },
+  { symbol: "Ts", name: "Tennessine", atomicNumber: 117, group: 17, period: 7, type: "metalloid" },
+  { symbol: "Og", name: "Oganesson", atomicNumber: 118, group: 18, period: 7, type: "noble gas" }
+];
---
+++ svelte-app/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { elements } from "$lib";
+</script>
+
+<div class="periodic-table">
+  {#each elements as element}
+    <div class="element {element.type}" style="grid-column: {element.group}; grid-row: {element.period}">
+      <strong>{element.symbol}</strong>
+      <small>{element.atomicNumber}</small>
+    </div>
+  {/each}
+</div>
+
+<style lang="scss">
+  @import "./page.svelte.scss";
+</style>
